e0908f2196a0d8215d754a61cb16e7ced7c29b3e
Agu Chux <agu.chux@yahoo.com>
2022-07-01 23:43:20 +0100

    Second update

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const cors = require('cors')
-const helmet = require('helmet')
-const logger = require('./modules/logger')
 
 server.use(cors())
-server.use(helmet())
 server.unsubscribe(express.json())
 server.use(express.urlencoded({ extended: true }))
 
@@ -17,7 +14,7 @@ server.get("/", (req, res) => {
     res.status(200).json({ data: [], error: 1 })
 })
 
-require('./modules/database')
+// require('./modules/database')
 const port = process.env.PORT || 5000
 server.listen(port, async () => {
     console.log(`Server running on ${port}`)
