ad40f879e5f2390db3ae973727ce1f9ae926ddf7
Agu Chux <agu.chux@yahoo.com>
2022-07-02 00:42:42 +0100

    Beautiful ways

--- models/logs.js
+++ models/logs.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-const sequence = require('mongoose-sequence')
+const sequence = require('mongoose-sequence')(mongoose)
 
 
 const logSchema = new mongoose.Schema({
     log: String,
     date: {
         type: Date,
-        default: Dat.now
+        default: Date.now
     }
 })
 
