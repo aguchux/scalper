08b23cdf1267e01ef0bf90cb41aed85f7e1cd04b
Agu Chux <agu.chux@yahoo.com>
2022-07-02 01:53:36 +0100

    Wrapping it up

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  proxy: {
+    '/api/v1': {
+      target: 'https://golojan-scalper.herokuapp.com/',
+      changeOrigin: true,
+      secure: true,
+      ws: true,
+    }
+  }
 })
